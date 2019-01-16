a34a33d2b46fbbcfcab200737220a4c2370e9b2a
sandr463 <stanish2000@gmail.com>
2019-01-16 17:54:43 +0200

    Set Webhooks, deleted parce func

--- telegram_stuff/telegram_main.py
+++ telegram_stuff/telegram_main.py
@@ -1,57 +1,25 @@
-from telegram_stuff.settings import auth_token, main_url
-import requests
-from datetime import datetime
-from flask import Flask, request
+import json
+from flask import Flask, request, jsonify
+from flask_sslify import SSLify
 
 
 app = Flask(__name__)
+ssl = SSLify(app)
 
 
-def get_updates():
-    method = 'getUpdates'
-    response = requests.get(main_url + method)
-    response_json = response.json()['result']
-    return response_json
-
-
-last_update = get_updates()[-1]
-
-
-def get_channel_id():
-
-    return last_update['channel_post']['chat']['id']
-
-
-def get_author_signature():
-
-    return last_update['channel_post']['author_signature']
-
-
-def get_converted_date_of_message():
-    timestamp = last_update['channel_post']['date']
-    converted_time = datetime.fromtimestamp(timestamp)
-    return converted_time
-
-
-def get_text_of_message():
-
-    return last_update['channel_post']['text']
-
-
-# print(get_channel_id())
-# print(get_author_signature())
-# print(get_converted_date_of_message())
-# print(get_text_of_message())
+def write_json(data, filename='answer.json'):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
 
 
 @app.route('/', methods=['POST'])
 def index():
     if request.method == 'POST':
-        pass
+        r = request.get_json()
+        write_json(r)
+        return jsonify(r)
     return '<h1>Hello world</h1>'
 
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000)
-
-
+    app.run()
