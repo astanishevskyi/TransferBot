236ff4ac2e990bc1aa954f0963592bd0eb4ab4d1
sandr463 <stanish2000@gmail.com>
2019-01-19 17:24:12 +0200

    Added get-set func in redis_db

--- redis_db/redis_main.py
+++
@@ -1,8 +0,0 @@
-from redis import Redis
-import redis
-
-
-def main():
-    conn_pool = redis.ConnectionPool(host='localhost', port=6379, db=2)
-    redis_cli = redis.Redis(connection_pool=conn_pool)
-
--- telegram_stuff/telegram_main.py
+++ telegram_stuff/telegram_main.py
@@ -1,6 +1,7 @@
 import json
 from flask import Flask, request, jsonify
 from flask_sslify import SSLify
+from redis_db.getter_setter import get_all, get_link_on_slack, redis_main
 
 
 app = Flask(__name__)
@@ -12,11 +13,19 @@ def write_json(data, filename='answer.json'):
         json.dump(data, f, indent=2, ensure_ascii=False)
 
 
+def get_json():
+    with open("answer.json", "r") as read_file:
+        data = json.load(read_file)
+        return data
+
+
 @app.route('/', methods=['POST'])
 def index():
     if request.method == 'POST':
         r = request.get_json()
         write_json(r)
+        redis_main(get_json())
+
         return jsonify(r)
     return '<h1>Hello world</h1>'
 
